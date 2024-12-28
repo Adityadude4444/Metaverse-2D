bbdf9bc0531817f6d0f7897b2bf61011bc8e1344
Adityadude4444 <22pa1a0539@vishnu.edu.in>
2024-12-28 21:45:34 +0530

    1st test case Auth passed

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -2,8 +2,6 @@ import express from "express";
 import { router } from "./routes/v1";
 const app = express();
 
-import client from "@repo/db/client";
-
 app.use("/api/v1", router);
 
 app.listen(5000);
--- apps/http/src/routes/v1/index.ts
+++ apps/http/src/routes/v1/index.ts
@@ -2,34 +2,84 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
-import { SignupSchema } from "../../types";
+import { SigninSchema, SignupSchema } from "../../types";
 import client from "@repo/db/client";
+import { compare, hash } from "../../scrypt";
+import { JWT_SECRET } from "../../config";
+
+const jwt = require("jsonwebtoken");
 export const router = Router();
 
 router.post("/signup", async (req, res) => {
-  const parsedata = SignupSchema.safeParse(req.body);
-  if (!parsedata.success) {
-    res.status(400).json({
-      message: "Invalid Data",
-    });
+  console.log("inside signup");
+  // check the user
+  const parsedData = SignupSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    console.log("parsed data incorrect");
+    res.status(400).json({ message: "Validation failed" });
     return;
   }
+
+  const hashedPassword = await hash(parsedData.data.password);
+
   try {
     const user = await client.user.create({
       data: {
-        username: parsedata.data.username,
-        password: parsedata.data.password,
-        role: parsedata.data.type === "admin" ? "Admin" : "User",
+        username: parsedData.data.username,
+        password: hashedPassword,
+        role: parsedData.data.type === "admin" ? "Admin" : "User",
       },
     });
-  } catch (error) {}
-
-  res.json({
-    message: "Signup",
-  });
+    res.json({
+      userId: user.id,
+    });
+  } catch (e) {
+    console.log("erroer thrown");
+    console.log(e);
+    res.status(400).json({ message: "User already exists" });
+  }
 });
 
-router.post("/signup", (req, res) => {});
+router.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(403).json({ message: "Validation failed" });
+    return;
+  }
+
+  try {
+    const user = await client.user.findUnique({
+      where: {
+        username: parsedData.data.username,
+      },
+    });
+
+    if (!user) {
+      res.status(403).json({ message: "User not found" });
+      return;
+    }
+    const isValid = await compare(parsedData.data.password, user.password);
+
+    if (!isValid) {
+      res.status(403).json({ message: "Invalid password" });
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        role: user.role,
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      token,
+    });
+  } catch (e) {
+    res.status(400).json({ message: "Internal server error" });
+  }
+});
 
 router.get("/elements", (req, res) => {});
 
